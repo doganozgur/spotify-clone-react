c1baa082f4d27f56af83238eeea2450044a39a00
Doğan Özgür <doganozgur034@gmail.com>
2023-03-20 20:46:18 +0300

    fixed download page back button display on bigger screens

--- src/components/Sidebar/DownloadButton.tsx
+++ src/components/Sidebar/DownloadButton.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import { Icon } from "../../utils/Icons";
 
 export default function DownloadButton() {
   return (
-    <div className="flex items-center px-6 h-12 space-x-4 w-100 bg-black text-sm text-link hover:text-white pb-{100%] cursor-pointer">
+    <Link
+      to="download"
+      className="flex items-center px-6 h-12 space-x-4 w-100 bg-black text-sm text-link hover:text-white pb-{100%] cursor-pointer"
+    >
       <Icon name="download" />
       <span className="text-sm">Uygulamayı Yükle</span>
-    </div>
+    </Link>
   );
 }
--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 import Hero from "../assets/spotify.png";
 import DownloadModal from "../components/DownloadModal";
+import useScreenWidth from "../hooks/useScreenWidth";
 import { Icon } from "../utils/Icons";
 
 export default function Download() {
   let [isOpen, setIsOpen] = useState(false);
+
+  const screenWidth = useScreenWidth();
 
   let navigate = useNavigate();
 
@@ -20,9 +23,11 @@ export default function Download() {
 
   return (
     <section className="max-w-2xl mx-auto text-center mb-4">
-      <button className="block mb-6" onClick={() => navigate(-1)}>
-        <Icon name="arrowLeft" size={24} />
-      </button>
+      {screenWidth < 768 && (
+        <button className="block mb-6" onClick={() => navigate(-1)}>
+          <Icon name="arrowLeft" size={24} />
+        </button>
+      )}
 
       <img className="w-full" src={Hero} alt="Spotify App View" />
       <h2 className="text-center sm:text-3xl text-xl font-semibold pb-6 leading-relaxed">
